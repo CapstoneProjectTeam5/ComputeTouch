3485ac5ab959ff64643de0e02f0e459504719d9c
cdj2073 <79151181+cdj2073@users.noreply.github.com>
2021-04-21 02:58:41 +0900

    not compute well...

---
+++ ComputeTouch/src/computeTouchMeasurements/Stroke.java
@@ -0,0 +1,24 @@
+package computeTouchMeasurements;
+
+import java.util.ArrayList;
+
+@SuppressWarnings("serial")
+public class Stroke extends ArrayList<Point>
+{	
+	/*
+	public List<Point> Stroke = new ArrayList<Point>();
+	
+	public static final int Count = 0;
+
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return Stroke.size();
+	}
+
+	public void add(Point point) {
+		// TODO Auto-generated method stub
+		Stroke.add(point);
+	}
+	*/
+}
